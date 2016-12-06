67b662771d032a2529f0a28951548cdb8dc3d36a
hhhhh <hhhhh@qq.com>
2016-12-06 13:07:14 +0800

    mysql

---
+++ mysql_myblogyii/myblog_reply.sql
@@ -0,0 +1,2 @@
+INSERT INTO `myblog_reply` VALUES (1, '写的不错', 'aaa', '2016-11-13 17:39:35');
+INSERT INTO `myblog_reply` VALUES (2, '好，很好', 'bbb', '2016-11-3 22:40:01');
---
+++ mysql_myblogyii/myblog_tails.sql
@@ -0,0 +1,3 @@
+INSERT INTO `myblog_tails` VALUES (1, '霍赖黛~杰Q赖', '2016-3-16', 'img/dummies/54x54.gif');
+INSERT INTO `myblog_tails` VALUES (2, '不知道叫什么名字啊', '2016-11-7', 'img/dummies/54x54b.gif');
+INSERT INTO `myblog_tails` VALUES (3, '美丽大方善良的我', '2014-10-31', 'img/dummies/54x54c.gif');
